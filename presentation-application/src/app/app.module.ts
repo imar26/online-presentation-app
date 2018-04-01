3ab866caff4cc919670b44ebb582da4b913af6df
Bhumika <khatri.bh@husky.neu.edu>
2018-04-01 01:25:54 -0400

    Initial SLide Displayed

--- presentation-application/src/app/app.module.ts
+++ presentation-application/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SlideAreaComponent } from './slide-area/slide-area.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SlideAreaComponent
+   
   ],
   imports: [
     BrowserModule
